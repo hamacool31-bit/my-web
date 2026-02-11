12eef9dd54c7539b567826eb0d19ff70b073e196
hamacool31-bit <hamacool31@gmail.com>
2026-02-12 02:30:36 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $host="localhost";
 $user="root";
 $pass="";
 $db="login";
-$conn=mysqli_connect ($host,$user,$pass,$db);
+$conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
 }
